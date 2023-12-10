037d721b9e75be437a29e3a91094c944a05c2f94
Jose Rojas <joseyx041199@gmail.com>
2023-12-10 03:42:35 -0400

    Added services to handle api call and auth

---
+++ src/app/services/axios.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import axios, { AxiosInstance } from 'axios';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AxiosService {
+  private httpClient: AxiosInstance;
+
+  constructor(cookieService: CookieService) {
+    this.httpClient = axios.create({
+      baseURL: 'http://127.0.0.1:8000/api/',
+      timeout: 10000,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    this.httpClient.interceptors.request.use(function (config) {
+      const token = cookieService.get('accessToken');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
+  }
+
+  get(url: string) {
+    return this.httpClient.get(url);
+  }
+
+  post(url: string, data: any) {
+    return this.httpClient.post(url, data);
+  }
+}
