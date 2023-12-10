8001128e169f608cf1186c5f7127232b2c31c07a
Jose Rojas <joseyx041199@gmail.com>
2023-12-10 03:43:27 -0400

    Added login component

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
+import { TestComponent } from '../../nav/test.component';
+
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [TestComponent, ReactiveFormsModule, RouterModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  constructor(private authService: AuthService, private router: Router) {
+    if (this.authService.isUserLoggedIn()) {
+      this.router.navigate(['/']);
+    }
+  }
+
+  applyForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+  async submitLogin() {
+    try {
+      if (
+        !this.applyForm.getError('email') &&
+        !this.applyForm.getError('password')
+      ) {
+        const response = await this.authService.login(
+          this.applyForm.value.email ?? '',
+          this.applyForm.value.password ?? ''
+        );
+        console.log(response);
+        this.router.navigate(['/']);
+      } else {
+      }
+    } catch (error) {}
+  }
+}
