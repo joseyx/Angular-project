64ca185c5eb0ed17c63e6b25947cdd68ceca7a95
Jose Rojas <joseyx041199@gmail.com>
2023-12-10 03:40:24 -0400

    Added multiple routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { TestComponent } from './nav/test.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { PasswordForgotComponent } from './auth/password-forgot/password-forgot.component';
+import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: TestComponent,
+    title: 'Test',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login',
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Register',
+  },
+  {
+    path: 'forgot-password',
+    component: PasswordForgotComponent,
+    title: 'Forgot Password',
+  },
+  {
+    path: 'reset-password/:token',
+    component: ResetPasswordComponent,
+    title: 'Reset Password',
+  },
+];
