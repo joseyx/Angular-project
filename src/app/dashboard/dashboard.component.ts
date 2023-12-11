fa69c0173467a78e497c15e68e4271e820e52ab3
Jose Rojas <joseyx041199@gmail.com>
2023-12-10 20:04:30 -0400

    Added dashboard base component

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { DashboardNavComponent } from './components/dashboard-nav/dashboard-nav.component';
+import { DashboardMainComponent } from './components/dashboard-main/dashboard-main.component';
+import { DashboardSidebarComponent } from './components/dashboard-sidebar/dashboard-sidebar.component';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    DashboardNavComponent,
+    DashboardMainComponent,
+    RouterOutlet,
+    DashboardSidebarComponent,
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class DashboardComponent {}
