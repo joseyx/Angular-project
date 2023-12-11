61f38a9ce030e4a4990e7e318e09ae8f0cc78b5a
Jose Rojas <joseyx041199@gmail.com>
2023-12-11 01:53:29 -0400

    Added dashboard profile component

---
+++ src/app/dashboard/components/dashboard-profile/dashboard-profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-profile.component.html',
+  styleUrl: '../../dashboard.component.css',
+})
+export class DashboardProfileComponent {}
