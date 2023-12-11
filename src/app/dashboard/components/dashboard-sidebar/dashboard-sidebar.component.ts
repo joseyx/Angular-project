8fd05aec45d5eed15cfb18ae7df13b10685689cc
Jose Rojas <joseyx041199@gmail.com>
2023-12-10 20:05:38 -0400

    Added dashboard sidebar component

---
+++ src/app/dashboard/components/dashboard-sidebar/dashboard-sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-sidebar.component.html',
+  styleUrl: '../../dashboard.component.css',
+})
+export class DashboardSidebarComponent {}
